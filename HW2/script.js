16957d868e021629e5711d4429e24e13c73179cb
Kaisha Meichell Gerhardt <70034111+KaishaGerhardt@users.noreply.github.com>
2024-01-29 18:35:41 -0700

    Add files via upload

---
+++ HW2/script.js
@@ -0,0 +1,32 @@
+/*
+
+3 of your favorite websites
+        1. https://radio.garden/listen/khdv-fm-107-9-the-drive/RNLWsR90
+        2. https://xrafstar.monster/games/twine/river/
+        3. https://arkadia.xyz/   
+
+3 of your favorite games if you play any - they can be board games, etc.
+
+        1. Baldur's Gate III
+        2. Dragon Age Origins
+        3. Minecraft?
+
+
+3 of your favorite artists and their cultural importance
+
+        1. Marina Abramović - 
+           In 1974, she invited audience members to do whatever they wanted to her using any of the 72 items she provided: 
+           pen, scissors, chains, axe, loaded pistol, and others. To see how soceity would interact.
+           The performance ceased when audience members grew too aggressive.
+
+        2. Vincent van Gogh -
+            Impressionist painter who In just over a decade he created approximately 2100 artworks, 
+            most of them in the last two years of his life.
+
+        3. Banksy - 
+           An anonymous graffiti artist. 
+           He is known for his anti-authoritarian art, often done in public places. He came to notice as a freehand graffiti artist.
+*/
+// Date: 1 /29/ 24
+
+console.log("JavaScript Rules!");
